1bccfd0ba8f460bcc4247415980d5e236b910229
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-11-05 21:20:18 -0600

    Create routes in the app

---
+++ src/pages/crear-producto.tsx
@@ -0,0 +1,8 @@
+export default function CrearProducto() {
+  return (
+    <div>
+      <h1>Seccion Crear Producto</h1>
+    </div>
+  );
+}
+
---
+++ src/pages/ver-producto.tsx
@@ -0,0 +1,7 @@
+export default function VerProducto() {
+    return (
+      <div>
+        <h1>Seccion Ver Producto</h1>
+      </div>
+    );
+  }
